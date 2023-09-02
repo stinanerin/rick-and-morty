38e4b34367d6d1ed4da2d562383e39305af3d35e
stormstina <stina.nerin@gmail.com>
2023-09-02 16:17:49 +0200

    Small fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { CharacterListContextProvider } from "./components/context/CharacterListContext";
 import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <CharacterListContextProvider>
-            <App />
-        </CharacterListContextProvider>
+        <App />
     </React.StrictMode>
 );
